0ab5318e0e92b6aad907dec0f2704594f207f55e
Brian kimanzi <migelbrian3@gmail.com>
2026-07-21 03:48:08 +0300

    New code update

--- FloatMinusInt.java
+++ FloatMinusInt.java
@@ -11,10 +11,6 @@ public class FloatMinusInt {
         int myInt = (int) value2;
         return myInt;
     }
-     public static void main(String[] args) {
-         FloatMinusInt fmi = new FloatMinusInt();
-        System.out.println(fmi.subtractIntFromDoubleAndReturnDouble(5.5, 2));
-        System.out.println(fmi.subtractIntFromDoubleAndReturnInt(5.5, 2));
-    }
+    
 
 }
