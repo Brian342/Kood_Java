1bc8bbde05c2786dee8b14bba97e37821ae7c59e
Brian kimanzi <migelbrian3@gmail.com>
2026-07-25 23:39:57 +0300

    new code update

--- javaPractice/CypherTool.java
+++ javaPractice/CypherTool.java
@@ -6,68 +6,140 @@ public class CypherTool {
     public static void main(String[] args){
         InputData input =  getInput();
 
-        // encryption code
-        String res = ""; // placeholder for the message string
+            // encryption code
+            String res = ""; // placeholder for the message string
 
-        if(input.operation == 1){
-            res = switch(input.choice){
-               case 1 -> encryptRot13(input.message);
-               case 2 -> encryptAtbash(input.message);
-               default -> "Invalid Choice of encyption";
-           };
+            if (input.operation == 1) {
+                res = switch (input.choice) {
+                    case 1 -> encryptRot13(input.message);
+                    case 2 -> encryptAtbash(input.message);
+                    default -> "Invalid Choice of encyption";
+                };
 // decryption of the message
-        }else if(input.operation == 2){
-            res = switch (input.choice){
-                case 1 -> decryptRot13(input.message);
-                case 2 -> decryptAtbash(input.message);
-                default -> "Invalid choice of decyption";
-            };
-        }
+            } else if (input.operation == 2) {
+                res = switch (input.choice) {
+                    case 1 -> decryptRot13(input.message);
+                    case 2 -> decryptAtbash(input.message);
+                    default -> "Invalid choice of decyption";
+                };
+            }
 
-    }
-    public static InputData getInput(){
+            System.out.println(res);
+        };
+
+    public static InputData getInput() {
         Scanner scanner = new Scanner(System.in);
-        int operation;
-        int choice;
+        int operation = 0;
+        int choice = 0;
         System.out.println("Welcome to the Cypher Tool! ");
 
-        System.out.print("""
-                Select operation:
-                1. Encrypt
-                2. Decrypt
-                :\s""");
-        operation = scanner.nextInt();
+        while (true) {
+            System.out.print("""
+                    Select operation:
+                    1. Encrypt
+                    2. Decrypt
+                    :>\s""");
+            if (scanner.hasNextInt()) {
+                operation = scanner.nextInt();
 
-        System.out.print("""
-                Select Cypher:
-                1. ROT13
-                2. Atbash
-                3. encryption_of_your_choice
-                :\s""");
+                if (operation == 1 || operation == 2) {
+                    break;
+                } else {
+                    System.out.println("Invalid choice! Please select 1 or 2.\n");
+                }
 
-        choice = scanner.nextInt();
+            } else {
+                System.out.println("Please Enter a valid number!\n");
+                scanner.next();
+            }
+        }
+
+        while (true) {
+            System.out.print("""
+                    Select Cypher:
+                    1. ROT13
+                    2. Atbash
+                    3. encryption_of_your_choice
+                    :>\s""");
+
+            if (scanner.hasNext()) {
+                choice = scanner.nextInt();
+
+                if (choice == 1 || choice == 2 || choice == 3) {
+                    break;
+                } else {
+                    System.out.println("Invalid choice! Please select 1, 2 or 3.\n");
+                }
+
+            } else {
+                System.out.println("Please Enter a valid number!\n");
+                scanner.next();
+            }
+        }
+
 
         scanner.nextLine();
 
-        System.out.print("Enter your message: ");
-        String message = scanner.nextLine();
-        System.out.print(message);
+    System.out.print("Enter your message: ");
+    String message = scanner.nextLine();
+    System.out.println(message);
+
 
         return new InputData(operation, choice, message);
     }
     public static String encryptRot13(String s){
+        if( s==null||s.isEmpty()){
+            System.exit(0);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
 
-        return "Hello Rot13";
+            // handle all the uppercase letters
+            if(c >= 'A' && c<= 'Z'){
+                if(c <= 'M'){
+                    c += 13;
+                }else{
+                    c -= 13;
+                }
+            }
+
+            // Handling all the Lowercase letters
+            if(c >= 'a' && c<= 'z'){
+                if(c <= 'm'){
+                    c += 13;
+                }else{
+                    c -= 13;
+                }
+            }
+            sb.append(c);
+        }
+
+        System.out.println();
+        System.out.println("Message after encrypted");
+        return sb.toString();
     }
     public static String encryptAtbash(String s){
-        return "Hello AtBash";
+        s = " Hello AtBash";
+
+        System.out.println();
+        System.out.println("Message after encrypted");
+        return s;
     }
 
     public static String decryptRot13(String s){
-        return "Hello decyptRot";
+        s = " Hello decryptRot";
+
+        System.out.println();
+        System.out.println("Message after Decrypted");
+        return s;
     }
     public static String decryptAtbash(String s){
-        return "Hello decyptAtBash";
+        s = " Hello decryptAtBash";
+
+        System.out.println();
+        System.out.println("Message after Decrypted");
+        return s;
     }
 }
 
